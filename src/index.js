244835aa423efb282653309a8c68a11ea21631bb
alan <me@alansoto.info>
2018-01-02 21:48:49 +1000

    call reducer from store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
+
+const store = createStore(reducer);
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
